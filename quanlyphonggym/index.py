f98002c686ec79ff7df61a9b556d2207cfec615f
pduydaden <ledophucduy1976@gmail.com>
2025-12-20 00:21:44 +0700

    1

--- quanlyphonggym/index.py
+++ quanlyphonggym/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -6,9 +6,19 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=['get', 'post'])
 def login():
-    return render_template("login.html")
+    err_msg = None
+    if request.method.__eq__('POST'):
+        email=request.form.get("email")
+        pswd=request.form.get("pswd")
+
+        if email=="user@gmail.com" and pswd=="123":
+            return redirect("/")
+        else:
+            err_msg="tai khoan hoac mat khau khong dung"
+
+    return render_template("login.html", err_msg=err_msg)
 
 if __name__ == "__main__":
     with app.app_context():
